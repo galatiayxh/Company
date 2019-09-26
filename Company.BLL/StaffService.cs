2bfadb78a2a8708946b52ffcde00fd5d060a388f
俊凯 丁 <h2722877@live.com>
2019-09-26 11:56:50 +0800

    添加项目文件。

---
+++ Company.BLL/StaffService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.IBLL;
+using Company.IDAL;
+using Company.Model;
+using Company.DALContainer;
+
+
+namespace Company.BLL
+{
+    public partial class StaffService : BaseService<Staff>, IStaffService
+    {
+        private IStaffDAL StaffDAL = DALContainer.Container.Resolve<IStaffDAL>();
+        public override void SetDal()
+        {
+            Dal = StaffDAL;
+        }
+    }
+}
